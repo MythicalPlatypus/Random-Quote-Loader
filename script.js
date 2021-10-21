363c7407826fe0cfc730e1db682604bc7a769e5a
Jefferson <JeffersonClarke1995@outlook.com>
2021-10-21 00:17:38 -0400

    Third Revision - Added a Copy to Clipboard button and Tooltip (with a Timeout to reset message for Clipboard.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
+const clipboardButton = document.getElementById('cpy-clipboard');
 const newQuoteBtn = document.getElementById('new-quote');
 
 function newLocalQuote() {
@@ -21,21 +22,34 @@ function newLocalQuote() {
     }
     // Check if the quote is a long quote, If so apply long-quote class.
     if (quote.text.length >= 100) {
-        console.log("it's more than 100!")
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quote.text;
-    console.log(quoteText.innerHTML);
 }
 
 function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
     window.open(twitterUrl, '_blank');
+    const tooltip = document.getElementById("twitterTooltip");
+    tooltip.innerHTML = 'Tweeted!';
+}
+
+function textToClipboard() {
+    const dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = quoteText.textContent + ' - ' + authorText.textContent;
+    dummy.select();
+    navigator.clipboard.writeText(dummy.value);
+    document.body.removeChild(dummy);
+    const tooltip = document.getElementById("clipboardTooltip");
+    tooltip.innerHTML = "Copied: " + dummy.value;
+    setTimeout(() => {tooltip.innerHTML = "Copy to Clipboard"}, 1000);
 }
 
 newQuoteBtn.addEventListener("click", newLocalQuote);
+clipboardButton.addEventListener("click", textToClipboard)
 twitterBtn.addEventListener("click", tweetQuote)
 newLocalQuote();
 
